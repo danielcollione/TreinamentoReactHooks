f9ac26a79b9b83e6809ccb8e9a637e1a8ef354f8
danielcollione <daniel.collione22@gmail.com>
2020-05-13 14:47:57 -0300

    commit Conversor de Moedas
    commit final do conversor de Moedas

--- conversor-de-moedas/src/conversor-moedas.js
+++ conversor-de-moedas/src/conversor-moedas.js
@@ -1,78 +1,138 @@
 import React, { useState } from 'react';
 import './conversor-moedas.css';
-import { 
-  Jumbotron, Button, Form, Col, Spinner, Alert, Modal 
+import {
+  Jumbotron, Button, Form, Col, Spinner, Alert, Modal
 } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 import ListarMoedas from './listar-moedas';
-
+import axios from 'axios';
 
 function ConversorMoedas() {
+
+  const FIXER_URL = 'http://data.fixer.io/api/latest?access_key=eba7130a5b2d720ce43eb5fcddd47cc3';
 
   const [valor, setValor] = useState('1');
   const [moedaDe, setMoedaDe] = useState('BRL');
   const [moedaPara, setMoedaPara] = useState('USD');
+  const [exibirSpinner, setExibirSpinner] = useState(false);
+  const [formValidado, setFormValidado] = useState(false);
+  const [exibirModal, setExibirModal] = useState(false);
+  const [resultadoConversao, setResultadoConversao] = useState('');
+  const [exibirMsgErro, setExibirMsgErro] = useState(false);
 
-  function handleValor(event){
-    setValor(event.target.value.replace(/\D/g, '')); //replace(D) - verifica dados não numericos e substitui por ''.
+  function handleValor(event) {
+    setValor(event.target.value.replace(/\D/g, ''));
   }
 
-  function handleMoedaDe(event){
+  function handleMoedaDe(event) {
     setMoedaDe(event.target.value);
   }
 
-  function handleMoedaPara(event){
+  function handleMoedaPara(event) {
     setMoedaPara(event.target.value);
+  }
+
+  function handleFecharModal(event) {
+    setValor('1');
+    setMoedaDe('BRL');
+    setMoedaPara('USD');
+    setFormValidado(false);
+    setExibirModal(false);
+  }
+
+  function converter(event) {
+    event.preventDefault();
+    setFormValidado(true);
+    if (event.currentTarget.checkValidity() === true) {
+      setExibirSpinner(true);
+      axios.get(FIXER_URL)
+        .then(res => {
+          const cotacao = obterCotacao(res.data);
+          if (cotacao) {
+            setResultadoConversao(`${valor} ${moedaDe} = ${cotacao} ${moedaPara}`);
+            setExibirModal(true);
+            setExibirSpinner(false);
+            setExibirMsgErro(false);
+          } else {
+            exibirErro();
+          }
+        }).catch(err => exibirErro());
+    }
+  }
+
+  function obterCotacao(dadosCotacao) {
+    if (!dadosCotacao || dadosCotacao.success !== true) {
+      return false;
+    }
+    const cotacaoDe = dadosCotacao.rates[moedaDe];
+    const cotacaoPara = dadosCotacao.rates[moedaPara];
+    const cotacao = (1 / cotacaoDe * cotacaoPara) * valor;
+    return cotacao.toFixed(2);
+  }
+
+  function exibirErro() {
+    setExibirMsgErro(true);
+    setExibirSpinner(false);
   }
 
   return (
     <div>
-      <h1>Conversor de Moedas</h1>
-      <Alert variant="danger" show={false}>
+      <h1>Conversor de moedas</h1>
+      <Alert variant="danger" show={exibirMsgErro}>
         Erro obtendo dados de conversão, tente novamente.
       </Alert>
       <Jumbotron>
-        <Form>
+        <Form onSubmit={converter} noValidate validated={formValidado}>
           <Form.Row>
-
-            <Col sn="3"> 
-              <Form.Control placeholder="0" value={valor} onChange={handleValor} required /> 
+            <Col sm="3">
+              <Form.Control
+                placeholder="0"
+                value={valor}
+                onChange={handleValor}
+                required />
             </Col>
-
-            <Col sn="3">
-              <Form.Control as="select" value={moedaDe} onChance={handleMoedaDe}><ListarMoedas/></Form.Control>
+            <Col sm="3">
+              <Form.Control as="select"
+                value={moedaDe}
+                onChange={handleMoedaDe}>
+                <ListarMoedas />
+              </Form.Control>
             </Col>
-
-            <Col sn="1" className="text-center" style={{paddingTop: '5px'}}>
+            <Col sm="1" className="text-center" style={{paddingTop:'5px'}}>
               <FontAwesomeIcon icon={faAngleDoubleRight} />
             </Col>
-
-            <Col sn="3">
-              <Form.Control as="select" value={moedaPara} onChance={handleMoedaPara}><ListarMoedas/></Form.Control>  
+            <Col sm="3">
+              <Form.Control as="select"
+                value={moedaPara}
+                onChange={handleMoedaPara}>
+                <ListarMoedas />
+              </Form.Control>
             </Col>
-
-            <Col sn="2">
-              <Button variant="success" type="submit">
-                <Spinner animation="border" size="sm"></Spinner>
-                Converter
+            <Col sm="2">
+              <Button variant="success" type="submit" data-testid="btn-converter">
+                <span className={exibirSpinner ? null : 'hidden'}>
+                  <Spinner animation="border" size="sm" />
+                </span>
+                <span className={exibirSpinner ? 'hidden' : null}>
+                  Converter
+                </span>
               </Button>
             </Col>
-
           </Form.Row>
         </Form>
-        <Modal show={false}>
-
+        <Modal show={exibirModal} onHide={handleFecharModal} data-testid="modal">
           <Modal.Header closeButton>
             <Modal.Title>Conversão</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            Resultado da Conversão aqui...
+            {resultadoConversao}
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="success">Nova Conversão</Button>
+            <Button variant="success" onClick={handleFecharModal}>
+              Nova conversão
+            </Button>
           </Modal.Footer>
-
         </Modal>
       </Jumbotron>
     </div>
--- conversor-de-moedas/src/conversor-moedas.test.js
+++ conversor-de-moedas/src/conversor-moedas.test.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { render } from '@testing-library/react';
 import ConversorMoedas from './conversor-moedas';
+import { render, fireEvent } from '@testing-library/react';
+import axiosMock from 'axios'
 
-it('deve renderizar o componente sem erros', () => {
-  const div = document.createElement('div');
-  ReactDom.render(<ConversorMoedas/>, div);
-  ReactDom.unmountComponentAtNode(div);
+describe('Teste do componente de conversão de moedas', () => {
+
+  it('deve renderizar o componente sem erros', () => {
+    const div = document.createElement('div');
+    ReactDom.render(<ConversorMoedas/>, div);
+    ReactDom.unmountComponentAtNode(div);
+  });
+
+  it('deve simular uma conversão de moedas', async () => {    
+    const { findByTestId, getByTestId } = render(<ConversorMoedas />);
+    axiosMock.get.mockResolvedValueOnce({
+      data: {success: true, rates: { BRL: 4.564292, USD: 1.101049 }}
+    });
+    fireEvent.click(getByTestId('btn-converter'));
+    const modal = await findByTestId('modal');
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(modal).toHaveTextContent('1 BRL = 0.24 USD');   
+  });
+
 });
