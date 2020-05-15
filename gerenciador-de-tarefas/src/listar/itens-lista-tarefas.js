00ab69a007a8a85bff6e7135bb4641e174329b5e
danielcollione <daniel.collione22@gmail.com>
2020-05-15 11:50:50 -0300

    Commit Almoço
    Commit do almoço dia 15/05

--- gerenciador-de-tarefas/src/listar/itens-lista-tarefas.js
+++ gerenciador-de-tarefas/src/listar/itens-lista-tarefas.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { A } from 'hookrouter';
 import ConcluirTarefa from './concluir-tarefa';
+import RemoverTarefa from './remover-tarefa';
 
 function ItensListaTarefas(props) {
 
@@ -29,7 +30,10 @@ function ItensListaTarefas(props) {
             className={tarefa.concluida ? 'hidden' : 'btn btn-warning btn-sm'}>
             <FontAwesomeIcon icon={faEdit} />
           </A>
-          
+          &nbsp;
+          <RemoverTarefa
+            tarefa={tarefa}
+            recarregarTarefas={props.recarregarTarefas} />
         </td>
       </tr>
     )
--- gerenciador-de-tarefas/src/listar/listar-tarefas.js
+++ gerenciador-de-tarefas/src/listar/listar-tarefas.js
@@ -1,28 +1,76 @@
 import React, { useState, useEffect } from 'react';
 import { A } from 'hookrouter';
-import { Table } from 'react-bootstrap';
+import { Table, Form } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import ItensListaTarefas from './itens-lista-tarefas';
+import Paginacao from './paginacao';
+import Ordenacao from './ordenacao';
 
 
 function ListarTarefas() {
 
+    const ITEMS_POR_PAG = 3;
+
     const [tarefas, setTarefas] = useState([]);
     const [carregarTarefas, setCarregarTarefas] = useState(true);
+    const [totalItems, setTotalItems] = useState(0);
+    const [paginaAtual, setPaginaAtual] = useState(1);
+    const [ordenarAsc, setOrdenarAsc] = useState(false);
+    const [ordenarDesc, setOrdenarDesc] = useState(false);
+    const [filtroTarefa, setFiltroTarefa] = useState('');
+
 
     useEffect(() => {
         function obterTarefas() {
+
             const tarefasDb = localStorage['tarefas'];
-            let listarTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
-            setTarefas(listarTarefas);
-            console.log(listarTarefas);
+            let listaTarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
+            //filtrar
+            listaTarefas = listaTarefas.filter(
+                t => t.nome.toLowerCase().indexOf(filtroTarefa.toLowerCase()) === 0
+            );
+            //ordenar
+            if (ordenarAsc){
+                listaTarefas.sort((t1, t2) => (t1.nome.toLowerCase() > t2.nome.toLowerCase()) ? 1 : -1);
+            } else if (ordenarDesc){
+                listaTarefas.sort((t1, t2) => (t1.nome.toLowerCase() < t2.nome.toLowerCase()) ? 1 : -1);
+            }
+            //paginar
+            setTotalItems(listaTarefas.length);
+            setTarefas(listaTarefas.splice((paginaAtual - 1) * ITEMS_POR_PAG, ITEMS_POR_PAG));
         }
+
         if(carregarTarefas){
             obterTarefas();
             setCarregarTarefas(false);
         }
-    }, [carregarTarefas]);
+    }, [carregarTarefas, paginaAtual, ordenarAsc, ordenarDesc]);
+
+    function handleMudarPagina(pagina){
+        setPaginaAtual(pagina);
+        setCarregarTarefas(true);
+    }
+
+    function handleOrdenar(event){
+        event.preventDefault();
+        if(!ordenarAsc && !ordenarDesc){
+            setOrdenarAsc(true);
+            setOrdenarDesc(false);
+        } else if (ordenarAsc) {
+            setOrdenarAsc(false);
+            setOrdenarDesc(true);
+        } else {
+            setOrdenarAsc(false);
+            setOrdenarDesc(false);
+        }
+        setCarregarTarefas(true);
+    }
+
+    function handleFiltrar(event){
+        setFiltroTarefa(event.target.value);
+        setCarregarTarefas(true);
+    }
 
     return (
         <div className="text-center">
@@ -32,13 +80,32 @@ function ListarTarefas() {
             <Table striped bordered hover responsive data-testid="tabela">
                 <thead>
                     <tr>
-                        <th>Tarefa</th>
+                        <th>
+                            <a href="/" onClick={handleOrdenar}>
+                                Tarefa 
+                                &nbsp; 
+                                <Ordenacao
+                                    ordenarAsc={ordenarAsc}
+                                    ordenarDesc={ordenarDesc} />
+                                </a>
+                        </th>
                         <th>
                             <A href="/cadastrar" className="btn btn-success btn-sm" data-testid="btn-nova-tarefa">
                                 <FontAwesomeIcon icon={faPlus} /> 
                                 &nbsp; Nova Tarefa
                             </A>
                         </th>
+                    </tr>
+                    <tr>
+                        <th>
+                            <Form.Control
+                                type="text"
+                                value={filtroTarefa}
+                                onChange={handleFiltrar}
+                                data-testid="txt-tarefa"
+                                className="filtro-tarefa" />
+                        </th>
+                        <th>&nbsp;</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -47,8 +114,12 @@ function ListarTarefas() {
                     recarregarTarefas={setCarregarTarefas} />
                 </tbody>
             </Table>
-            
-
+            <Paginacao 
+                totalItems={totalItems}
+                itemsPorPagina={ITEMS_POR_PAG}
+                paginaAtual={paginaAtual}
+                mudarPagina={handleMudarPagina}>
+            </Paginacao>
         </div>
     );
 }
