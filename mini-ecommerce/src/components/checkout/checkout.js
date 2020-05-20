c599a392be8b50c4bd057837c37f49ae76408fad
danielcollione <33942652+danielcollione@users.noreply.github.com>
2020-05-20 12:07:53 -0300

    Commit almoço
    commit almoço dia 20/05

--- mini-ecommerce/src/components/checkout/checkout.js
+++ mini-ecommerce/src/components/checkout/checkout.js
@@ -1,8 +1,229 @@
 import React from 'react';
+import { Form, Row, Col, Button, Jumbotron, Modal } from 'react-bootstrap';
+import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker, { registerLocale } from 'react-datepicker';
+import pt from 'date-fns/locale/pt';
+import PropTypes from 'prop-types';
+import ListarEstados from './listar-estados';
+import ListarCidades from './listar-cidades';
 
-function Checkout(){
+registerLocale('pt', pt);
+
+function Checkout(props){
     return (
-        <h1>Checkout</h1>
+        <Jumbotron
+            fluid
+            style={{margin: '10px'}}>
+
+            <h3 className="text-center">Finalizar Compra</h3>
+
+
+            <Form 
+                noValidate
+                style={{margin: '10px'}}>
+                    <Form.Group as={Row} controlId="email">
+                        <Form.Label column sm={3}>
+                            Email
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control 
+                                type="email"
+                                placeholder="Digite o seu email"
+                                name="email"
+                                data-testid="txt-email"/>
+                            <Form.Control.Feedback type="invalid">
+                                Digite um email válido.
+                            </Form.Control.Feedback>
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="nomeCompleto">
+                        <Form.Label column sm={3}>
+                            Nome completo
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control
+                                type="text"
+                                placeholder="Digite seu nome completo"
+                                name="nomeCompleto"
+                                data-testid="txt-nome-completo"/>
+                            <Form.Control.Feedback type="invalid">
+                                Digite o seu nome completo (mínimo 5 caracteres).
+                            </Form.Control.Feedback>
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="dataNascimento">
+                        <Form.Label column sm={3}>
+                            Data de nascimento
+                        </Form.Label>
+                        <Col sm={9}>
+                            <DatePicker 
+                                locale="pt"
+                                peekNextMonth
+                                showMonthDropdown
+                                showYearDropdown
+                                dropdownMode="select"
+                                dateFormat="dd/MM/yyyy"
+                                placeholderText="Selecione a data"
+                                withPortal />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="cpf">
+                        <Form.Label column sm={3}>
+                            CPF
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control
+                                type="text"
+                                placeholder="Digite o seu CPF"
+                                name="cpf"
+                                data-testid="txt-cpf" />
+                            <Form.Control.Feedback type="invalid">
+                                Digite um CPF válido.
+                            </Form.Control.Feedback>
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="endereco">
+                        <Form.Label column sm={3}>
+                            Endereço
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control
+                                type="text"
+                                placeholder="Digite o seu endereço"
+                                name="endereco"
+                                data-testid="txt-endereco" />
+                            <Form.Control.Feedback type="invalid">
+                                Digite o seu endereço.
+                            </Form.Control.Feedback>
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="estado">
+                        <Form.Label column sm={3}>
+                            Estado
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control
+                                as="select"
+                                name="estado"
+                                data-testid="txt-estado" >
+                                <ListarEstados />
+                            </Form.Control>
+                            <Form.Control.Feedback type="invalid">
+                                Digite o seu estado.
+                            </Form.Control.Feedback>
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="cidade">
+                        <Form.Label column sm={3}>
+                            Cidade
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control
+                                as="select"
+                                name="cidade"
+                                data-testid="txt-cidade" >
+                                <ListarCidades estado={''} />
+                            </Form.Control>
+                            <Form.Control.Feedback type="invalid">
+                                Digite a sua cidade.
+                            </Form.Control.Feedback>
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="cep">
+                        <Form.Label column sm={3}>
+                            CEP
+                        </Form.Label>
+                        <Col sm={9}>
+                            <Form.Control
+                                type="text"
+                                placeholder="Digite o seu CEP"
+                                name="cep"
+                                data-testid="txt-cep" />
+                            <Form.Control.Feedback type="invalid">
+                                Digite o seu CEP.
+                            </Form.Control.Feedback>
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="emailPromocional">
+                        <Form.Label column sm={12}>
+                            Deseja receber emails com promoções?
+                        </Form.Label>
+                        <Form.Check
+                            inline
+                            name="emailPromocional"
+                            type="radio"
+                            id="promocaoSim"
+                            value="S"
+                            label="Sim"
+                            style={{marginLeft: "15px"}}/>                           
+                        <Form.Check
+                            inline
+                            name="emailPromocional"
+                            type="radio"
+                            id="promocaoNao"
+                            value="N"
+                            label="Não"/>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="termosCondicoes">
+                        <Form.Check 
+                            name="termosCondicoes"
+                            label="Concordo com os termos e condições"
+                            style={{marginLeft: '15px'}}
+                            data-testid="check-termos-condicoes" />
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="finalizarCompra">
+                        <Col className="text-center" sm={12}>
+                            <Button
+                                type="submit"
+                                variant="success"
+                                data-testid="btn-finalizar-compra">
+                                    Finalizar compra
+                            </Button>
+                        </Col>
+                    </Form.Group>
+
+            </Form>
+
+            <Modal show={false} data-testid="modal-compra-sucesso">
+
+                <Modal.Header closeButton>
+                    <Modal.Title>Compra realizada com sucesso!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Um email de confirmação foi enviado com os detalhes da transação.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="success">Continuar</Button>
+                </Modal.Footer>
+
+            </Modal>
+
+            <Modal show={false} data-testid="modal-erro-comprar">
+
+                <Modal.Header closeButton>
+                    <Modal.Title>Erro ao processar pedido.</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Tente novamente em instantes.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="warning">Continuar</Button>
+                </Modal.Footer>
+
+            </Modal>
+
+
+        </Jumbotron>
     );
 }
 
